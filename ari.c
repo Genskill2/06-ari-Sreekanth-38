a2be1e094220993a3452df83f2bc0e311b59a129
Sreekanth-38 <84661591+Sreekanth-38@users.noreply.github.com>
2021-06-30 21:45:43 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -1,22 +1,23 @@
-#include<stdio.h>
-#include<cs50.h>
-int swap_max(int arr[], int l, int n){
-    int max=arr[n];
-    int pos=n;
-    for(int i=n;i<l;i++){
-      if(arr[i]>max){
-        max=arr[i];
-        pos=i;}}
-    arr[pos]=arr[n];
-    arr[n]=max;
-    
-}
-int ssort(int arr[], int l){
-    for(int i=0; i<l;i++){
-    swap_max(arr, l, i);}
-    printf("the sorted array is \n|");
-    for(int i=0; i<9;i++)
-    printf(" %i |",arr[i]);
-    printf("\n");
-    
-}
+#include <stdio.h>
+#include <assert.h>
+
+void swap_max(int arr[], int l, int n ){ 
+	int largest = arr[n];
+	int swap_no = largest;
+	for(int i=n;i<l;i++){
+	  if(arr[i]>largest)
+	    largest = arr[i];
+	    }
+	arr[n]= largest;
+	for(int i=n+1; i<l;i++){
+	  if (arr[i] == largest)
+	    arr[i] = swap_no;
+	    }
+	 }
+	 
+	 
+void ssort(int arr[], int l){
+	for(int i=0;i<l;i++){
+	  swap_max(arr,l,i);
+	  }
+	}
